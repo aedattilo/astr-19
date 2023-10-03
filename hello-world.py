4c6c40bfb4bd269d1f5f144dfe8ee0bf79ba209d
Anne Dattilo <anne.dattilo1@gmail.com>
2023-10-03 12:54:33 -0700

    Added a hello-world program.
    This repo is now much less boring!

---
+++ hello-world.py
@@ -0,0 +1,17 @@
+# This program will write
+# Hello World!
+
+# This function tell the
+# computer to print
+# "Hello World!"
+def print_hello_world():
+	print("Hello World!")
+
+# This defines our
+# main() function of
+# the program
+def main():
+	print_hello_world()
+
+if __name__=="__main__":
+	main()
